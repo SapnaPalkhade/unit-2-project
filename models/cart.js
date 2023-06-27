4d169135b702423ce1b3cf26c8ac4b8ff14ae083
Sapnapalkhade <sapnapalkhade@gmail.com>
2023-06-26 23:44:24 -0700

    cart schema created

---
+++ models/cart.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const {model, Schema } = require('mongoose')
+const mongoose = require('mongoose')
+
+const cartSchema = new mongoose.Schema({
+    user: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+    items: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item'}],
+    total: Number
+})
+
+const Cart = mongoose.model('Cart', cartSchema)
+
+module.exports = Cart
