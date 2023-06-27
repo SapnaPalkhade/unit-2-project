09cfbc2cd9f77e52eab96bc4010bc8e48798cf55
Sapnapalkhade <sapnapalkhade@gmail.com>
2023-06-27 16:37:15 -0700

    itemController created

---
+++ controllers/itemController.js
@@ -0,0 +1,51 @@
+const Item = require('../models/item')
+const User = require('../models/user')
+
+
+exports.create = async (req, res) => {
+    try {
+        req.body.user = req.user._id
+        const item = new Item(req.body)
+        await item.save()
+        res.json(item)
+    } catch (error) {
+       res.status(400).json({ message: error.message}) 
+    }
+}
+
+exports.showItem = async function(req, res){
+    try {
+        const item = await Item.findOne({_id: req.params.id })
+        res.json(item)
+    } catch (error) {
+        res.status(400).json({ message: error.message})   
+    }
+}
+
+exports.showAllItems = async (req, res) => {
+    try {
+      const items = await Item.find();
+      res.json(items);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  };
+  
+
+exports.updateItem = async (req, res) => {
+  try {
+    const item = await Item.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    res.json(item)
+  } catch (error) {
+    res.status(400).json({ message: error.message})  
+  }
+}
+
+exports.delete = async function(req, res){
+    try {
+        const item = await Item.findOneAndDelete({ _id: req.params.id })
+        res.sendStatus(204)
+    } catch (error) {
+        res.status(400).json({ message: error.message}) 
+    }
+}
